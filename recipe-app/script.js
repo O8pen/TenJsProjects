efa1e36472bb70cbfbdb0187d4d91ef64d9902e4
Burak Alp Yasar <burakalpyasar.o8pen@gmail.com>
2022-02-21 23:47:36 +0300

    2.02.13

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -8,10 +8,8 @@ async function getRandomMeal(){
 
 async function getMealById(id) {
     const meal = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-
 }
 
 async function getMealsBySeatch(term) {
     const meals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-
 }
